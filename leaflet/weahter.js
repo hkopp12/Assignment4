97252758d61b896df6ef6ebd081711e736a5ef18
hkopp12 <hkopp1@uwyo.edu>
2024-10-04 17:36:17 -0600

    weather

---
+++ leaflet/weahter.js
@@ -0,0 +1,28 @@
+var map = L.map('weathermap').setview([38, -95], 4);
+var basemapUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+var basemap = L.tileLayer(basemapUrl, {attrinuution: '&copy; <a href="http://' + 'www.openstreetmap.org/copyright">Open'})
+
+var radarUrl = 'https//mespmet.argon.iastate.edu/cgi-bin.wms/nexrad/n0r.cgi';
+var radarDisplayOptions = {
+    layers: 'nexrad-n0r-900913',
+    format: 'image/png',
+    transparent: true
+};
+var radar = L.tileLayer.wms(radarUrl, radarDisplayOptions).addTp(map);
+
+var weatherAlertsUrl = 'httpsL..api.weather.gov/alerts/active?region_tupe=land';
+$.getJSON(weatherAlertsUrl, funtion(data) {
+
+    L.geoJSON(data, {
+        style: funtion(feature){
+            var alertColor = 'orange';
+            if (feature.properties.severity === 'Severe') alertColor = 'red';
+            return { color: alertColor };
+        },
+        onEachFeature: function(feature, Layer) {
+            Layer.bindPopup(feature.properties.headline);
+        }
+    
+    }).addTo(map);
+
+})
